54331bee5e0e6af567f03521391cdae2d402c355
snezhanna.mineeva@gmail.com <snezhanna.mineeva@gmail.com>
2025-05-14 12:27:59 +0000

    int_orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,8 @@
+select 
+    orders_id,
+    date_date,
+    round(sm.margin+sh.shipping_fee-sh.log_cost-sh.ship_cost,2) as operational_margin
+from {{ref ("int_orders_margin")}} sm
+left join {{ref ("stg_raw__ship")}} sh
+using (orders_id)
+ORDER BY orders_id DESC
