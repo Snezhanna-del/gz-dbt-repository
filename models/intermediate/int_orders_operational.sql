5d9ffbaa72b55b06412c8b67cbc5ec60768c40ac
snezhanna.mineeva@gmail.com <snezhanna.mineeva@gmail.com>
2025-05-14 13:00:28 +0000

    finance_days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,11 @@
 select 
     orders_id,
     date_date,
-    round(sm.margin+sh.shipping_fee-sh.log_cost-sh.ship_cost,2) as operational_margin
+    round(sm.margin+sh.shipping_fee-sh.log_cost-sh.ship_cost,2) as operational_margin,
+    revenue,
+    quantity,
+    purchase_cost,
+    margin
 from {{ref ("int_orders_margin")}} sm
 left join {{ref ("stg_raw__ship")}} sh
 using (orders_id)
